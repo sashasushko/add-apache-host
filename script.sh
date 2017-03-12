6118fbd022c691d2117bfbcd323d20e5d3e3fb35
Sasha Sushko <sushko@outlook.com>
2017-03-12 11:29:33 +0300

    Translate comments to english

--- script.sh
+++ script.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-# Порядок действий
-# 0. Проверки
-# 1. Объявление переменных
-# 2. Создание папок "www" и "logs" хоста с индексным файлом
-# 3. Создание файла конфига по шаблону "apache.sample.conf"
-# 4. Добавление хоста
-# 5. Перезагрузка Апача
-# 6. Уведомление об окончании
+# Steps
+# 0. Checks
+# 1. Var
+# 2. Create "www" and "logs" folder
+# 3. Place index to host "www" folder
+# 4. Create Apache host config file by template "apache.sample.conf" and place it to Apache folder
+# 5. Add host
+# 6. Restart Apache
+# 7. End
 
 # 0.
-# Проверяем, что есть название хоста
+# Check that user entered a host name
 if [ -z "$1" ]
   then
     echo "Please, set host name as first argument and try again."
     exit
 fi
 
-# Проверяем, что название содержит только буквы, цифры и дефис
+# Check that host name contains only latin letters, numbers, dash and underscore
 if [[ $1 =~ ^[A-Za-z0-9_-]+$ ]]
   then
-    # ToDo: переписать условие так, чтобы не было else
+    # ToDo: Rewrite "if" construction so that remove "else" block
   else
-    echo "Host name must contain only letters, numbers, dash and underscore."
+    echo "Host name must contain only latin letters, numbers, dash and underscore."
 	exit
 fi
 
@@ -40,17 +41,20 @@ fi
 mkdir $HOME/Sites/$apache_host_name
 mkdir $HOME/Sites/$apache_host_name/www
 mkdir $HOME/Sites/$apache_host_name/logs
-sed "s/%hostname%/$apache_host_name/g" $curent_path/index.sample.html > $HOME/Sites/$apache_host_name/www/index.html
 
 # 3.
+sed "s/%hostname%/$apache_host_name/g" $curent_path/index.sample.html > $HOME/Sites/$apache_host_name/www/index.html
+
+# 4.
 sed -e "s/%username%/$user_name/g" -e "s/%hostname%/$apache_host_name/g" $curent_path/apache.sample.conf > $curent_path/$apache_host_name.dev.conf
 sudo mv $curent_path/$apache_host_name.dev.conf /etc/apache2/vhosts/
 
-# 4.
+# 5.
+# ToDo: Replace echo by something invisible to user
 echo "127.0.0.1 $apache_host_name.dev" | sudo tee -a /etc/hosts
 
-# 5.
+# 6.
 sudo apachectl restart
 
-# 6.
+# 7.
 echo "Host '$apache_host_name.dev' successfully added."
